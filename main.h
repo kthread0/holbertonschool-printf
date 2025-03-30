92ee5b2d94577f245f4b92fbe1994487ff22100a
glovek08 <gbrielbbarn@gmail.com>
2025-03-30 16:20:15 -0300

    [fix] missing handle_cases prototype in header file

--- main.h
+++ main.h
@@ -1,7 +1,9 @@
 #ifndef PRINTF_MAIN_H
 #define PRINTF_MAIN_H
+#include <stdarg.h>
 
 int _printf(const char *, ...);
+int handle_cases(const char *, va_list);
 int _putchar(char);
 int _print_int(int);
 int _strlen(char *);
