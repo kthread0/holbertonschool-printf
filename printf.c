4ea2e4cd32fc6ded445127e293a9d8beb6036ee8
glovek08 <gbrielbbarn@gmail.com>
2025-03-30 16:17:49 -0300

    [feat] created case handler function cause of line limit in printf.c

--- printf.c
+++ printf.c
@@ -3,26 +3,47 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to a format
- * @format: format string containing the directives
+ * _printf - Produces output according to a format
+ * @format: Format string containing directives
  *
- * Return: number of characters printed
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, acc = 0; /* acc = accumulator for each printed character */
+	int acc;
 
 	if (!format)
 		return (-1);
+
 	va_start(args, format);
+	acc = handle_cases(format, args);
+	va_end(args);
+
+	return (acc);
+}
+
+/**
+ * handle_cases - Handles format specifiers for _printf
+ * @format: Format string containing directives
+ * @args: va_list containing the value to print
+ *
+ * Return: Number of characters printed
+ */
+int handle_cases(const char *format, va_list args)
+{
+	int i = 0, acc = 0;
+
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
-			switch (format[++i])
+			i++;
+			if (!format[i])
+				return (-1);
+
+			switch (format[i])
 			{
-				case '\0': return (0);
 				case 'c':
 					acc += _putchar(va_arg(args, int));
 					break;
@@ -30,8 +51,6 @@ int _printf(const char *format, ...)
 					acc += _print_string(va_arg(args, char *));
 					break;
 				case 'i':
-					acc += _print_int(va_arg(args, int));
-					break;
 				case 'd':
 					acc += _print_int(va_arg(args, int));
 					break;
@@ -44,9 +63,11 @@ int _printf(const char *format, ...)
 			}
 		}
 		else
+		{
 			acc += _putchar(format[i]);
+		}
 		i++;
 	}
-	va_end(args);
 	return (acc);
 }
+
