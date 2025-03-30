6869a85512260c54bb80814d015c4d3d5a6582a7
glovek08 <gbrielbbarn@gmail.com>
2025-03-30 18:35:26 -0300

    [refactor] int to string conversion moved to second function in print_int.c

--- print_int.c
+++ print_int.c
@@ -1,36 +1,60 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
-* _print_int - converts a given integer into char,
-*	prints it and returns the total of characters printed.
-* @num: the given integer.
-*
-* Return: the sum of all printed digits.
-*/
-int _print_int(int num)
-{
-	int acc = 0, temp = num; /* acc: the string length */
-				/*temp = used to calculate num's last digit */
-	char *num_str = NULL; /* String to store converted integers. */
-	int i = 0;
 
+/**
+ * int_to_str - converts a given integer to  string.
+ * @num: the given integer.
+ *
+ * Return: pointer to the string.
+ */
+char *int_to_str(int num)
+{
+	int acc = 0, temp = num;
+	char *num_str;
+	int i;
+
+	if (num == 0)
+	{
+		num_str = malloc(2 * sizeof(char));
+		if (!num_str)
+			return (NULL);
+		num_str[0] = '0';
+		num_str[1] = '\0';
+		return (num_str);
+	}
 	while (temp > 0)
 	{
-		temp = temp / 10;
+		temp /= 10;
 		acc++;
 	}
-	num_str = malloc(sizeof(char) * acc + 1);
+	num_str = malloc(sizeof(char) * (acc + 1));
 	if (!num_str)
-		return (EXIT_FAILURE);
+		return (NULL);
 	temp = num;
 	num_str[acc] = '\0';
-	for (i = acc - 1; i >= 0; i--) /* -1 por el null terminator */
+	for (i = acc - 1; i >= 0; i--)
 	{
 		num_str[i] = (temp % 10) + '0';
 		temp /= 10;
 	}
-	for (i = 0; i < _strlen(num_str); i++)
+	return (num_str);
+}
+/**
+ * _print_int - prints an integer and returns the total characters printed.
+ * @num: the given integer.
+ *
+ * Return: the sum of all printed digits.
+ */
+int _print_int(int num)
+{
+	char *num_str;
+	int i, acc = 0;
+
+	num_str = int_to_str(num);
+	if (!num_str)
+		return (EXIT_FAILURE);
+	for (i = 0; num_str[i] != '\0'; i++)
 	{
 		_putchar(num_str[i]);
 		acc++;
