c3d4bb1ebfafba03228b7795cebd5ddcfb7d482b
glovek08 <gbrielbbarn@gmail.com>
2025-03-30 00:06:24 -0300

    [fix] correct decimal convertion handling

--- print_double.c
+++ print_double.c
@@ -5,20 +5,22 @@
 int _print_double(double num)
 {
     bool isNegative = false; /* Bool flag to handle negative numbers */
-    int whole_part = (int) num;  /* Separate whole from decimal part by typecasting trunc */
-    double decimal_part = num - whole_part;  /* Separates the decimal part. */
+    int whole_part = 0;  /* Separate whole from decimal part by typecasting trunc */
+    double decimal_part = 0;  /* Separates the decimal part. */
     int temp = 0; /* Holds the  */
     int whole_len = 0; /* holds the length of the whole part */
     int total_len = 0; /* the entire length of the number */
     char *str = (void *)0; /* Holds the converted number for printing */
+    int decimal_int = 0;
     int i = 0, j = 0;
 
     if (num < 0)
     {
         isNegative = true;
         num = num * -1;
-        total_len = 1; /* for the - symbol */
     }
+    whole_part = (int) num;
+    decimal_part = num - whole_part;
 
     /* Count the length of the whole part */
     temp = whole_part;
@@ -33,10 +35,13 @@ int _print_double(double num)
 
     str = malloc(total_len);
     if (!str)
-        return (EXIT_FAILURE);
+        return (-1);
 
     if (isNegative)
+    {
         str[i++] = '-';
+        total_len++;
+    }
 
     /* converting whole_part to string */
     for (j = whole_len - 1; j >= 0; j--)
@@ -48,18 +53,15 @@ int _print_double(double num)
     str[i++] = '.';
 
     /*converting decimal_part to string */
-    for (j = 0; j < 2; j++)
-    {
-        decimal_part = decimal_part * 10;
-        str[i++] = ((int)decimal_part % 10) + '0'; /* truncate decimal part */
-    }
+    decimal_part *= 100;
+    decimal_int = (int)(decimal_part + 0.5);
+    str[i++] = (decimal_int / 10) + '0';
+    str[i++] = (decimal_int % 10) + '0';
     str[i] = '\0';
 
     /* printing the converted double number */
     for (j = 0; j < _strlen(str); j++)
-    {
         _putchar(str[j]);
-    }
 
     free(str);
     return (total_len - 1);
